a5a109eb43099fc351577955ad2dd9735955ccb4
Carlos Eduardo <carloseduardobitencourte@gmail.com>
2026-02-09 15:01:35 -0300

    Criado variável curso

--- ex.py
+++ ex.py
@@ -2,9 +2,10 @@ print("Hello World")
 idade = 18
 print(idade)
 print("Testando Python")
+curso = "Python Desenvolvedor"
+print(curso)
 
 # Dicas:
 # Tab - Completa o comando
-# Bolinha branca no nome = N salvo
-# Ctrl + S = Save
-# Ctrl + L = Limpa o terminal
+# Bolinha branca no nome = N salvo / Ctrl + S = Save / Ctrl + L = Limpa o terminal
+# Pip = para mexer com as bibliotecas
